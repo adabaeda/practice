b2a22c83a203051258fc4c687c9be5b37587e27e
Ada Erlewine <ada.erlewine@gmail.com>
2025-01-23 12:42:29 -0800

    Iris graph

--- test.R
+++ test.R
@@ -1,7 +1,17 @@
-library(gitcreds)
+library(tidyverse)
+library(tidyverselibrary(gitcreds)
 git_sitrep()
 library(usethis)
+library(ggplot2)
 gitcreds_set()
 use_github()
 
 print("Hi")
+
+data()
+
+df <- iris %>% 
+  janitor::clean_names() 
+
+ggplot(df, aes(species, petal_length)) + geom_col()
+
