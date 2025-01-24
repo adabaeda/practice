cb8d9ea88d99001cd83839f433fa186ca7d9b899
Ada Erlewine <ada.erlewine@gmail.com>
2025-01-24 09:59:41 -0800

    Trying to delete files!

---
+++ Iris.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(usethis)
+library(ggplot2)
+
+gitcreds_set()
+use_github()
+
+print("Hi")
+
+data()
+
+df <- iris %>% 
+  janitor::clean_names() 
+
+
+ggplot(df, aes(species, sepal_length)) + geom_col()
+
+print("Something is happening")
